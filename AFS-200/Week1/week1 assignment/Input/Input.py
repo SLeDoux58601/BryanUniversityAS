9141a72e895f9d56dd055264f080a3e086a81aaf
Samantha R LeDoux <56769518+SLeDoux58601@users.noreply.github.com>
2020-09-24 13:27:44 -0600

    initial commit

---
+++ AFS-200/Week1/week1 assignment/Input/Input.py
@@ -0,0 +1,4 @@
+zipcode = int(input("Enter your zipcode: "))
+
+City = input ("What is the City you live in:")
+print("the city you live in is " + City)
---
+++ AFS-200/Week1/week1 assignment/print/message.py
@@ -0,0 +1,7 @@
+print ("I love python")
+
+name = input ("What is your name? ")
+print("your name is " + name)
+
+question = input ("do you love python? ")
+print =(""+question)
